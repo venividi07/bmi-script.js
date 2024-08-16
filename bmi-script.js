552aa1c952f716eb465b450c59aa71e5b2c79859
venividi07 <157637070+venividi07@users.noreply.github.com>
2024-08-16 18:35:07 +0300

    Add files via upload

---
+++ bmi-script.js
@@ -0,0 +1,21 @@
+// BMI Var 1.0
+let weightVadim = 79;
+let heightVadim = 1.69;
+let weightAlex = 92;
+let heightAlex = 1.95;
+let bmiVadim = weightVadim / heightVadim ** 2;
+let bmiAlex = weightAlex / heightAlex ** 2;
+console.log(bmiVadim, bmiAlex); 
+let markHighBMI= bmiVadim > bmiAlex;
+console.log(markHighBMI);
+
+// BMI Var 2.0
+let weightVadim2 = 95;
+let heightVadim2 = 1.88;
+let weightAlex2 = 85;
+let heightAlex2 = 1.76;
+let bmiVadim2 = weightVadim2 / heightVadim2 ** 2;
+let bmiAlex2 = weightAlex2 / heightAlex2 ** 2;
+console.log(bmiVadim2, bmiAlex2); 
+let markHighBMI2= bmiVadim2 > bmiAlex2;
+console.log(markHighBMI2);
